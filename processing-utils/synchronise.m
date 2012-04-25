b4c53fa75376658b9e7c6905b7716f2fdd420647
¨Adrian <Adrian.Cooke@utwente.nl>
2012-04-25 14:38:33 +0200

    Updating the synchronisation alogrithm. It is not tested yet.

--- processing-utils/synchronise.m
+++ processing-utils/synchronise.m
@@ -3,18 +3,21 @@ function [ data_1,data_2 ] = synchronise(metric_1,metric_2,data_1,data_2,Fs,theS
 %   Detailed explanation goes here
 
 display('Syncrohonising:');
-
-N=min(size(data_1,2),size(data_2,2));
+N1 = size(data_1,2);
+N2 = size(data_2,2);
+N=max(N1,N2);
 %Synchronise over a subset of the matrix's
-metric_1 = metric_1(theStart:size(metric_1,2));
-metric_2 = metric_2(theStart:size(metric_2,2));
-N = N-(theStart+1);
+metric_1 = metric_1(theStart:N1);
+metric_2 = metric_2(theStart:N2);
+N = N-(theStart-1);
+N1 = N1-(theStart-1);
+N2 = N2-(theStart-1);
 t = [0:1/Fs:N/Fs-1/Fs];
 figure
 subplot(2,1,1);
 hold on;
-plot(t,metric_1(1:N),'--k');
-plot(t,metric_2(1:N),'--m');
+plot(t(1:N1),metric_1(1:N1),'--k');
+plot(t(1:N2),metric_2(1:N2),'--m');
 title('Magnitude of Rotation Matrix change over time.');
 grid on;
 
@@ -30,17 +33,20 @@ R21=R;
 
 [max_12,max_lags_12]=max(R12)
 [max_21,max_lags_21]=max(R21)
+N=min(size(data_1,2),size(data_2,2));
 
 %Adjust input data.
 if (max(max_12,max_21) == max_12)
     display('Syncrohonising: Vicon ahead of IMU');
-    data_2 = data_2(N-max_lags_12:size(data_2,2));
-    metric_2 = metric_2(N-max_lags_12:size(metric_2,2));
+    data_2 = data_2(N2-max_lags_12:size(data_2,2));
+    metric_2 = metric_2(N2-max_lags_12:size(metric_2,2));
+    N2 = size(metric_2,2)
     theTitle = [' Synchronising: Vicon ahead of IMU by '...
         num2str(max_lags_12) ' '  num2str(max_12) ]
 else
-    data_1 = data_1(N-max_lags_21:size(data_1,2));
-    metric_1 = metric_1(N-max_lags_21:size(metric_1,2));
+    data_1 = data_1(N1-max_lags_21:size(data_1,2));
+    metric_1 = metric_1(N1-max_lags_21:size(metric_1,2));
+    N1 = size(metric_1,1)
     theTitle = ['Syncrohonising: IMU ahead of Vicon by ' ...
         num2str(max_lags_21) ' '  num2str(max_21)];
 end
@@ -56,9 +62,8 @@ end
 % data_1 = data_1(newStart:size(data_1,2));
 % data_2 = data_2(newStart:size(data_2,2));
 % 
-% N=min(size(data_1,2),size(data_2,2));
-% plot(t(newStart:N),metric_1(newStart:N),'--k')
-% plot(t(newStart:N),metric_2(newStart:N),'--m')
-% title(theTitle)
+plot(t(1:N1),metric_1,'--k')
+plot(t(1:N2),metric_2,'--m')
+title(theTitle)
 end
 
