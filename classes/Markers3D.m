662f4eeccf8ae9d2513c3daab48a7a17bfaf708f
¨Adrian <Adrian.Cooke@utwente.nl>
2013-01-08 12:14:11 +0100

    Reverted back to older Markers3D.m

--- classes/Markers3D.m
+++ classes/Markers3D.m
@@ -7,7 +7,52 @@ classdef Markers3D < ThreeD
     % maximal acceleration that is allowed for markers, otherwise markers are
     % considdered as outliers  % [m/s]
     methods (Static)
-       
+        function [vtm_t,gapArray] = dropQuaternions(rightBack,leftBack,front,N)
+            %MARKED MISSING MARKERS
+            CountDroppedMarkers = 0;
+            [rightBack,leftBack,front] = Markers3D.eraseNan(rightBack,leftBack,front);
+            
+            display('Check if the markers are well spaced');
+            
+            
+            %     rightBack
+            %             size(rightBack)
+            %             size(leftBack)
+            %             size(front)
+            %             N
+            parfor i = 1:N
+                if ~Markers3D.areTheMarkersWellSpaced(rightBack(i,1:3),...
+                        leftBack(i,1:3),...
+                        front(i,1:3));
+                    vtm_t{i} = -1;
+                    CountDroppedMarkers = CountDroppedMarkers +1;
+                else
+                    vtm = Markers3D(rightBack(i,1:3),...
+                        leftBack(i,1:3),front(i,1:3),rightBack(i,4:4));
+                    vtm_t{i} = vtm;
+                end
+            end
+            display(['Dropped Quaternions =', num2str(CountDroppedMarkers)]);
+            if ((CountDroppedMarkers/N)*100) > 5
+                warning('Markers3D:readDataVicon','Dropped Quaternions is more than 5 percent');
+            end
+            %REMOVE THE MARKED MARKERS
+            % If it is a class put it into the array, otherwise not (if it
+            % is -1)
+            vtm_tmp = {};
+            gapArray = [];
+            
+            for i = 1:N
+                if isa(vtm_t{i},'Markers3D')
+                    vtm_tmp = {vtm_tmp{:} vtm_t{i}};
+                else
+                    gapArray = [gapArray, i];
+                end
+            end
+            vtm_t = vtm_tmp;
+            display(['Array dropped Quaternions =', num2str(gapArray)]);
+        end
+        
         function [vtm_t,gapArray] = readDataVicon(filename,runName,...
                 rightBackName,leftBackName,frontName,varargin)
             %READDATA Reads the VICON three markers in
@@ -41,16 +86,16 @@ classdef Markers3D < ThreeD
                 [frontGap,foGapArray] = Markers3D.findFillGaps(front,...
                     50,1, 200);
                 
-
+                
                 N = length(rightBackGap);
                 display(['Interpolated markers in RB is =', num2str(size(rBGapArray, 1))]);
                 display(['Interpolated markers in LB is =', num2str(size(lBGapArray, 1))]);
                 display(['Interpolated markers in FO is =', num2str(size(foGapArray, 1))]);
                 InterpolatedMarkers = size(rBGapArray, 1)+size(lBGapArray, 1)+ ...
-                size(foGapArray, 1);
-            if ((InterpolatedMarkers/(3*N))*100) > 5
-                warning('Markers3D:readDataVicon','Interpolated markers in raw data is more than 5 percent');
-            end 
+                    size(foGapArray, 1);
+                if ((InterpolatedMarkers/(3*N))*100) > 5
+                    warning('Markers3D:readDataVicon','Interpolated markers in raw data is more than 5 percent');
+                end
                 
                 %               size(rightBackGap)
                 %               size(rightBack)
@@ -74,7 +119,7 @@ classdef Markers3D < ThreeD
             end
             
             [vtm_t,gapArray] = Markers3D.dropQuaternions(rightBack,leftBack,front,N);
-
+            
         end
         
         
